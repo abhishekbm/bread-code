28ccb01d7981713c1e2ab537d7719fb757f8cb45
abhishek.bm@oracle.com <abhishek.bm@oracle.com>
2017-03-11 21:18:04 +0530

    Initial functional commit.

---
+++ musicplayer/app/src/main/java/shakestudios/musicplayer/POJO/Song.java
@@ -0,0 +1,31 @@
+package shakestudios.musicplayer.POJO;
+
+/**
+ * Created by abbm on 3/9/2017.
+ */
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id = songID;
+        title = songTitle;
+        artist = songArtist;
+    }
+
+    public long getID() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+}
+
